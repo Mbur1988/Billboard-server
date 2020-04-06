cf9ec5eabdc1c1912d354a1b5e53acb6d55eda2a
Mark Burton <markburton88@live.com>
2020-04-06 14:13:40 +1000

    Projects merged into one

--- Billboard_Viewer/src/CustomExceptions/InvalidPortException.java
+++
@@ -1,9 +0,0 @@
-package CustomExceptions;
-
-public class InvalidPortException extends Exception {
-
-    public InvalidPortException() { super(); }
-
-    public InvalidPortException(String message) { super(message); }
-
-}
