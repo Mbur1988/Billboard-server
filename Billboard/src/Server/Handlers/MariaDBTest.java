162637ca3948719a4973d85bc316ca33d423aa00
Rhys <r_mapson@live.com.au>
2020-04-26 23:45:31 +1000

    Test

--- Billboard/src/Server/Handlers/MariaDBTest.java
+++ Billboard/src/Server/Handlers/MariaDBTest.java
@@ -1,17 +1,16 @@
 package Server.Handlers;
 
+import SerializableObjects.Billboard;
+import Tools.HashCredentials;
 import Tools.Log;
-import Tools.ObjectStreamer;
+import Tools.PropertyReader;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.io.*;
-import java.net.Socket;
-import java.sql.SQLException;
-import java.util.Random;
-import static Server.Server.mariaDB;
-
-public class MariaDBTest {
+import java.awt.*;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.*;
+import static java.lang.System.exit;
+import static java.lang.System.out;
 
 
-
-}
