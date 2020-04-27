adefb67037070d72262f6f8e1679990c91a64455
Rhys <r_mapson@live.com.au>
2020-04-27 23:30:19 +1000

    MariaDB Test Setup

--- Billboard/src/Server/Handlers/MariaDBTest.java
+++ Billboard/src/Server/Handlers/MariaDBTest.java
@@ -1,16 +1,34 @@
 package Server.Handlers;
 
-import SerializableObjects.Billboard;
 import Tools.HashCredentials;
 import Tools.Log;
 import Tools.PropertyReader;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import java.awt.*;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.*;
 import static java.lang.System.exit;
 import static java.lang.System.out;
+import Tools.DisplayImage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/*************************************************** This is the test class for the MariaDB****************************************************************
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *************************************************************************************************************************************************************/
+
+public class MariaDBTest {
 
 
+
+}
