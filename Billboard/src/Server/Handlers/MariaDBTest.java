182168491faef9cf060175f624c3698706baadb7
Rhys <r_mapson@live.com.au>
2020-04-28 14:23:39 +1000

    for mitch to see

--- Billboard/src/Server/Handlers/MariaDBTest.java
+++ Billboard/src/Server/Handlers/MariaDBTest.java
@@ -8,6 +8,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.sql.*;
 import static java.lang.System.exit;
 import static java.lang.System.out;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import Tools.DisplayImage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,6 +31,21 @@ import org.junit.jupiter.api.Test;
 
 public class MariaDBTest {
 
+String name = "test1";
+String msg = "msgtest1";
+String info = "dbtest";
+String picURL = "test1";
+byte[] picData = new byte[200];
+String msgColour = "Redtest";
+String backColour = "Greentest";
+String infoColour = "Bluetest";
+
+@BeforeEach
+@Test
+public void billboarAdd() {
+
+
+}
 
 
 }
