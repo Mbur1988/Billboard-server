93d4d6284b41f219a31f146f5ddbf5acb10aa4c8
Rhys <r_mapson@live.com.au>
2020-04-28 14:46:50 +1000

    test2

--- Billboard/src/Server/Handlers/MariaDBTest.java
+++ Billboard/src/Server/Handlers/MariaDBTest.java
@@ -1,19 +1,7 @@
 package Server.Handlers;
-
-import Tools.HashCredentials;
-import Tools.Log;
-import Tools.PropertyReader;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.sql.*;
-import static java.lang.System.exit;
-import static java.lang.System.out;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import Tools.DisplayImage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import java.sql.SQLException;
 /*************************************************** This is the test class for the MariaDB****************************************************************
  *
  *
@@ -28,24 +16,19 @@ import org.junit.jupiter.api.Test;
  *
  *
  *************************************************************************************************************************************************************/
-
 public class MariaDBTest {
-
-String name = "test1";
-String msg = "msgtest1";
-String info = "dbtest";
-String picURL = "test1";
-byte[] picData = new byte[200];
-String msgColour = "Redtest";
-String backColour = "Greentest";
-String infoColour = "Bluetest";
-
-@BeforeEach
-@Test
-public void billboarAdd() {
-
-
-}
-
-
+    byte[] testBytes = new byte[]{0, 1, 2};
+    MariaDB undertest;
+    @BeforeEach
+    void NewMariaDB() {
+        undertest = new MariaDB();
+    }
+    /**
+     *
+     */
+    @Test
+    public void AddTester() throws SQLException {
+        undertest.Billboards.addBillboard("name", "msg", "info", "picURL", testBytes, "msgColour", "backColour", "infoColour"){
+        }
+    }
 }
