bb64032c5d45f6e8e322dcbc496d32deff0abc1e
Rhys <r_mapson@live.com.au>
2020-05-07 23:59:45 +1000

    User Tests 1 -6
    tests for add user

--- Billboard/src/Server/Handlers/MariaDBTest.java
+++ Billboard/src/Server/Handlers/MariaDBTest.java
@@ -11,7 +11,9 @@ import java.lang.reflect.InvocationTargetException;
 import java.sql.*;
 import static java.lang.System.exit;
 import static java.lang.System.out;
-/*************************************************** This is the test class for the MariaDB****************************************************************
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/*************************************************** This is the test class for the billboards database in MariaDB*******************************************
  *First set of Tests will check multiple instances of the addBillboard method, with confirmation in the log confirming that particular billboard was added.
  *Each test will call getBillboard method to confirm that the billboard was added to the database.
  * Second set of Tests will check multiple instances of deleteBillboard method, with confirmation in the log confirming that the billboard was deleted
@@ -31,6 +33,8 @@ public class MariaDBTest {
 
     byte[] testBytes = new byte[]{0, 1, 2};
     MariaDB undertest;
+    byte[] salt = HashCredentials.CreateSalt();
+
 
     @BeforeEach
     void NewMariaDB() {
@@ -44,8 +48,9 @@ public class MariaDBTest {
     //**
     @Test
     public void AddTester1() throws SQLException {
+        undertest.billboards.checkForBillboard("test1");
         undertest.billboards.addBillboard("test1", "msg", "info", "picURL", testBytes, "msgColour", "backColour", "infoColour");{
-            undertest.billboards.getBillboard();
+        undertest.billboards.getBillboard();
         }
     }
     //**
@@ -114,8 +119,65 @@ public class MariaDBTest {
         undertest.billboards.getBillboard();
     }
 
+/*************************************************** This is the test class for the users database in MariaDB****************************************************************
+
+ *A range of tests to determine viability of mariaDB user methods. The Methods tested range from adding users, getting user passwords,
+ * deleting users. The assert true function is used to determine test success.
+ * Test 1-3 will add 3 different users to the database, with different usernames, passswords and access levels.
+ * Tests 4-? will then retrieve different information from the database concerning use details.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *************************************************************************************************************************************************************/
+
+//**
+//Test1 Adds a user to the database, user1, with password1, level 1 access.
+//**
+
+@Test
+    public void userTest1() throws SQLException{
+    undertest.users.AddUser("user1", "password1", 1, salt );
+    assertTrue(true);
+}
+
+//**
+//Test2 Adds a user to the database, user2, with password2, level 2 access.
+//**
+
+
+@Test
+    public void userTest2() throws SQLException{
+        undertest.users.AddUser("user2", "password2", 2, salt );
+        assertTrue(true);
+    }
+
+//**
+//Test3 Adds a user to the database, user3, with password3, level 3 access.
+//**
+
+@Test
+public void userTest3() throws SQLException{
+    undertest.users.AddUser("user3", "password3", 3, salt );
+    assertTrue(true);
+}
 
 
 
+@Test
+    public void getPasswordTest1() throws SQLException {
+    undertest.users.GetUserPassword("user1");
+    assertTrue(true);
+}
 
+@Test
+    public void getPasswordTest2() throws SQLException {
+    undertest.users.GetUserPassword("user1");
+    assertTrue(true);
+}
 }
