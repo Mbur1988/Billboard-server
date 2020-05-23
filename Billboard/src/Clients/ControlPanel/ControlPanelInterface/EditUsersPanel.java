3eb6bfbdbd132e33af42a8c87338fdb1b0b6b0c7
Mark Burton <markburton88@live.com>
2020-05-23 21:12:42 +1000

    EditUsersPanel class properly commented

--- Billboard/src/Clients/ControlPanel/ControlPanelInterface/EditUsersPanel.java
+++ Billboard/src/Clients/ControlPanel/ControlPanelInterface/EditUsersPanel.java
@@ -12,6 +12,7 @@ import static Clients.ControlPanel.ControlPanel.*;
 
 public class EditUsersPanel extends ControlPanelInterface {
 
+    // Variables required by the class
     private static JLabel lbl_password;
     private static JLabel lbl_message;
     private static JTextField tf_username;
@@ -26,75 +27,85 @@ public class EditUsersPanel extends ControlPanelInterface {
     private static JButton b_add;
     private static JButton b_save;
 
+    /**
+     * The main method of the edit user class populates the GUI page with all required objects
+     */
     public static void editUserScreen() {
 
         editUserPanel.setLayout(null);
 
         // Add title labels
+        // Title label for user details
         JLabel label_editUser = new JLabel("User details");
         label_editUser.setBounds(0,0,350,50);
         label_editUser.setFont(new Font("Courier", Font.PLAIN, 50));
         editUserPanel.add(label_editUser);
 
+        // Title label for the list of users
         JLabel lbl_users = new JLabel("List of all users");
         lbl_users.setBounds(screenWidth/2 - 100, 0, 400, 50);
         lbl_users.setFont(new Font("Courier", Font.PLAIN, 50));
         editUserPanel.add(lbl_users);
 
-        // Add labels
+        // Create labels
         JLabel lbl_username = new JLabel("Username");
         lbl_password = new JLabel("Password");
         JLabel lbl_confirm = new JLabel("Confirm Password");
         JLabel lbl_privileges = new JLabel("Privileges");
         lbl_message = new JLabel("");
 
+        // Add labels to the panel
         addLabel(editUserPanel, lbl_username, 10, 100, 300, 50);
         addLabel(editUserPanel, lbl_password, 10, 150, 300, 50);
         addLabel(editUserPanel, lbl_confirm, 10, 200, 300, 50);
         addLabel(editUserPanel, lbl_privileges, 10, 250, 300, 50);
         addLabel(editUserPanel, lbl_message, 190, 500, 340, 50);
 
-        // Add text fields
+        // Create text field
         tf_username = new JTextField();
+        // Add text field to the panel
         addTextfield(editUserPanel, tf_username, 190, 105, 300, 40);
 
-        // Add password fields
+        // Create password fields
         tf_password = new JPasswordField();
         tf_confirm = new JPasswordField();
 
+        // Add password field to the panel
         addPasswordField(editUserPanel, tf_password, 190, 155, 300, 40);
         addPasswordField(editUserPanel, tf_confirm, 190, 205, 300, 40);
 
-        // Add checkboxes
+        // Create checkboxes
         cb_createNew = new JCheckBox("Create New Billboard");
         cb_editBoard = new JCheckBox("Edit Existing Billboard");
         cb_schedule = new JCheckBox("Schedule Billboards");
         cb_editUsers = new JCheckBox("Edit Users");
 
+        // Add checkboxes to the panel
         addCheckBox(editUserPanel, cb_createNew, 190, 250, 300, 50);
         addCheckBox(editUserPanel, cb_editBoard, 190, 300, 300, 50);
         addCheckBox(editUserPanel, cb_schedule, 190, 350, 300, 50);
         addCheckBox(editUserPanel, cb_editUsers, 190, 400, 300, 50);
 
-        // Add default list model
+        // Create and add a default list model
         model = new DefaultListModel();
         model.addAll(lists.users);
 
-        // Add JList
+        // Create a new JList
         list = new JList(model);
 
-        // Add JScrollPane
+        // Create and add JScrollPane
         JScrollPane scrollPane = new JScrollPane(list);
         scrollPane.setBounds(screenWidth/2 - 100, 100, 300, 350);
         editUserPanel.add(scrollPane);
 
-        // Add buttons
+        // Create buttons
         b_add = new JButton("Add");
         b_save = new JButton("Save");
         JButton b_clear = new JButton("Clear");
         JButton b_load = new JButton("Edit");
         JButton b_delete = new JButton("Delete");
 
+        // Add buttons to panel
         addButton(editUserPanel, b_add, 190, 450, 150, 30);
         addButton(editUserPanel, b_save, 190, 450, 150, 30);
         b_save.setVisible(false);
@@ -104,58 +115,69 @@ public class EditUsersPanel extends ControlPanelInterface {
 
         // Handle button events
         b_add.addActionListener(event -> addUser());
-
         b_save.addActionListener(event -> saveUser());
-
         b_clear.addActionListener(event -> clearFields());
-
         b_load.addActionListener(event -> loadUser());
-
         b_delete.addActionListener(event -> deleteUser());
     }
 
+    /**
+     * Adds a new user to the database
+     */
     private static void addUser() {
+        // get user input
         String username = tf_username.getText();
         String password = String.valueOf(tf_password.getPassword());
         String confirm = String.valueOf(tf_confirm.getPassword());
+        // Check that all fields are populated
         if (username.equals("") || password.equals("") || confirm.equals((""))) {
             lbl_message.setText("All fields must be populated");
             return;
         }
+        // check that the password fields match
         if (!password.equals(confirm)) {
             resetFields();
             lbl_message.setText("Password fields must match");
             return;
         }
+        // hash the password
         password = HashCredentials.Hash(password);
+        // get access level as integer
         int access = UserAccess.bool2dec(
                 cb_createNew.isSelected(),
                 cb_editBoard.isSelected(),
                 cb_schedule.isSelected(),
                 cb_editUsers.isSelected());
+        // populate the user class instance
         User newUser = new User(username, password, access);
+        // set the action request to the server
         user.setAction("addUser");
-        // Attempt connection to server
+        // attempt connection to the server
         if (AttemptConnect()) {
             // Try a login attempt
             try {
                 // Send user object to server
                 objectStreamer.Send(user);
+                // Send new user object to server
                 objectStreamer.Send(newUser);
-                // Await returned object from server
+                // Await confirmation that the user was added successfully
                 if (dis.readBoolean()) {
+                    // add new user to the list of the current user's and re-sort it alphabetically
                     lists.users.add(newUser.getUsername());
                     Collections.sort(lists.users);
                     model.clear();
                     model.addAll(lists.users);
+                    // display confirmation message to the user and post log confirmation
                     lbl_message.setText("User added");
                     Log.Confirmation("New user added successfully");
                 }
+                // If user not added then display message to the user
                 else {
                     lbl_message.setText("User already exists");
                     Log.Error("Error when attempting to add new user");
                 }
             }
+            // catch any unanticipated exceptions and print to console
             catch (IOException e) {
                 e.printStackTrace();
                 Log.Error("Add user attempt request failed");
@@ -167,26 +189,36 @@ public class EditUsersPanel extends ControlPanelInterface {
         else {
             Log.Error("Unable to connect to server");
         }
+        // clear all user input fields
         resetFields();
     }
 
+    /**
+     * Saves any changes made to an existing user
+     */
     private static void saveUser() {
+        // get user input
         String password = String.valueOf(tf_password.getPassword());
         String confirm = String.valueOf(tf_confirm.getPassword());
+        // check that the password fields match
         if (!password.equals(confirm)) {
             resetFields();
             lbl_message.setText("Password fields must match");
             return;
         }
+        // get access level as integer
         int access = UserAccess.bool2dec(
                 cb_createNew.isSelected(),
                 cb_editBoard.isSelected(),
                 cb_schedule.isSelected(),
                 cb_editUsers.isSelected());
+        // return if there are no changes to be made
         if (access == user.getAccess() && password.equals("")) { return; }
+        // ensure there is a password entered and hash it
         if (!password.equals("")) {
             password = HashCredentials.Hash(password);
         }
+        // set the action request to the server
         user.setAction("editUser");
         // Attempt connection to server
         if (AttemptConnect()) {
@@ -194,20 +226,26 @@ public class EditUsersPanel extends ControlPanelInterface {
             try {
                 // Send user object to server
                 objectStreamer.Send(user);
+                // send the username of the user to edit
                 dos.writeUTF(tf_username.getText());
+                // send the password to edit
                 dos.writeUTF(password);
+                // send the access level to edit
                 dos.write(access);
-                // Await returned object from server
+                // Await confirmation from server
                 if (dis.readBoolean()) {
+                    // display confirmation message to the user and post log confirmation
                     lbl_message.setText("User edited");
                     Log.Confirmation("User edited successfully");
                     user.setAccess(access);
                 }
+                // If user not edited then display message to the user
                 else {
                     lbl_message.setText("User not edited");
                     Log.Error("Error when attempting to edit user");
                 }
             }
+            // catch any unanticipated exceptions and print to console
             catch (IOException e) {
                 e.printStackTrace();
                 Log.Error("Edit user attempt request failed");
@@ -219,11 +257,16 @@ public class EditUsersPanel extends ControlPanelInterface {
         else {
             Log.Error("Unable to connect to server");
         }
+        // set add button visible
         b_save.setVisible(false);
         b_add.setVisible(true);
+        // clear all user input fields
         resetFields();
     }
 
+    /**
+     * method used to reset the user input fields, clear the message and set add button visible
+     */
     private static void clearFields() {
         b_save.setVisible(false);
         b_add.setVisible(true);
@@ -231,6 +274,9 @@ public class EditUsersPanel extends ControlPanelInterface {
         lbl_message.setText("");
     }
 
+    /**
+     * Loads the credentials of the selected user from the database via the server
+     */
     private static void loadUser() {
         String username = (String) list.getSelectedValue();
         user.setAction("getAccess");
@@ -239,8 +285,11 @@ public class EditUsersPanel extends ControlPanelInterface {
             try {
                 // Send user object to server
                 objectStreamer.Send(user);
+                // send the username of the user to load
                 dos.writeUTF(username);
+                // read the access level of the user
                 boolean[] key = UserAccess.dec2bool(dis.read());
+                // populate user inout fields with the credentials of the user
                 tf_username.setEnabled(false);
                 tf_username.setText(username);
                 tf_password.setText("");
@@ -253,12 +302,14 @@ public class EditUsersPanel extends ControlPanelInterface {
                 lbl_password.setText("New Password");
                 cb_editUsers.setEnabled(!username.equals(user.getUsername()));
             }
+            // catch any unanticipated exceptions and print to console
             catch (IOException e) {
                 e.printStackTrace();
                 Log.Error("Failed to retrieve user credentials");
             }
             // Disconnect from server
             AttemptDisconnect();
+            // clear message to the user
             lbl_message.setText("");
         }
         // Post message to user if unable to connect to server
@@ -267,6 +318,9 @@ public class EditUsersPanel extends ControlPanelInterface {
         }
     }
 
+    /**
+     * Deletes the selected user from the database provided that it is not scheduled
+     */
     private static void deleteUser() {
         String username = (String) list.getSelectedValue();
         if (username.equals(user.getUsername())) {
@@ -280,18 +334,24 @@ public class EditUsersPanel extends ControlPanelInterface {
             try {
                 // Send user object to server
                 objectStreamer.Send(user);
+                // send the username of the user to delete
                 dos.writeUTF(username);
+                // await confirmation that the user has been successfully deleted
                 if (dis.readBoolean()) {
+                    // remove user from list
                     lists.users.remove(username);
                     model.removeElement(username);
+                    // display message to the user
                     lbl_message.setText("User deleted");
                     Log.Confirmation("User successfully deleted");
                 }
+                // Post message to user if unable to delete user
                 else {
                     lbl_message.setText("User not deleted");
                     Log.Error("Error when attempting to delete user");
                 }
             }
+            // catch any unanticipated exceptions and print to console
             catch (IOException e) {
                 e.printStackTrace();
                 Log.Error("Failed to delete user");
@@ -303,9 +363,13 @@ public class EditUsersPanel extends ControlPanelInterface {
         else {
             Log.Error("Unable to connect to server");
         }
+        // clear all user input fields
         resetFields();
     }
 
+    /**
+     * reset the user input fields to their initial status
+     */
     private static void resetFields() {
         tf_username.setEnabled(true);
         tf_username.setText("");
