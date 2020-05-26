95058f3c1ed603cdd475ab25c9d02ab571579719
Shane <shane.daunton@connect.qut.edu.au>
2020-05-26 21:42:26 +1000

    Auto update duration value
    No longer need to remove focus from the duration text field. Auto updates

--- Billboard/src/Clients/ControlPanel/ControlPanelTools/DurationSetter.java
+++ Billboard/src/Clients/ControlPanel/ControlPanelTools/DurationSetter.java
@@ -1,7 +1,12 @@
 package Clients.ControlPanel.ControlPanelTools;
 
 import java.awt.event.*;
+import java.text.Format;
+import java.text.NumberFormat;
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
 import static Clients.ControlPanel.ControlPanelInterface.ControlPanelInterface.schedulePanel;
 import static Clients.ControlPanel.ControlPanelInterface.ControlPanelInterface.screenWidth;
 import static Clients.ControlPanel.ControlPanelTools.Tools.*;
@@ -18,8 +23,6 @@ public class DurationSetter {
         JTextField tf_mins = new JTextField("duration (mins)");
         addTextfield(schedulePanel, tf_mins, ((screenWidth / 3)), 300, 160, 40);
 
-
-        // Watch for focus gain or lost on the text field to determine when to set the value.
         tf_mins.addFocusListener(new FocusListener() {
             @Override
             public void focusGained(FocusEvent e) {
@@ -27,10 +30,16 @@ public class DurationSetter {
             }
             @Override
             public void focusLost(FocusEvent e) {
-                // Only allow integer inputs for duration
+
+            }
+        });
+
+        tf_mins.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
                 try {
                     mins = Integer.parseInt(tf_mins.getText());
-                    lbl_duration.setText("Set duration: " + mins);
+
 
                 } catch (NumberFormatException i) {
                     if (tf_mins != null) {
@@ -39,8 +48,31 @@ public class DurationSetter {
                                 "Input Error", JOptionPane.ERROR_MESSAGE);
                     }
                 }
+                lbl_duration.setText("Set duration: " + mins);
+            }
 
+            @Override
+            public void removeUpdate(DocumentEvent e) {     // This part seems to create some errors.
+                mins = Integer.parseInt(tf_mins.getText()); // It's just meant to update the label when
+                lbl_duration.setText("Set duration: " + mins); // values are removed.       
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                try {
+                    mins = Integer.parseInt(tf_mins.getText());
+
+
+                } catch (NumberFormatException i) {
+                    if (tf_mins != null) {
+                        JOptionPane.showMessageDialog(schedulePanel,
+                                "Please set duration in number of minutes.",
+                                "Input Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+                lbl_duration.setText("Set duration: " + mins);
             }
         });
+
     }
 }
