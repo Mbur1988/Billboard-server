94ff1aa76a7819618c048111cd494dba666592b6
mitch-traynor <mitchtraynor@gmail.com>
2020-05-27 18:11:01 +1000

    Completed the documentation of Schedule

--- Billboard/src/SerializableObjects/Schedule.java
+++ Billboard/src/SerializableObjects/Schedule.java
@@ -5,17 +5,29 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-
+/**
+ *      Schedule
+ *      This class is used in other classes to store and create a schedule to be used in the database and to implement the different Billboards at different times for durations.
+ *      This class contains -
+ *      String - a name for the schedule which is used to identify the schedule
+ *      String - Billboard name, used to identify the Billboard  to be displayed
+ *      Local Date - Used to get the date and set the date of the Billboard to be displayed
+ *      Local Time - Used to set the time accuratly to display the image.
+ *      Duration - Used to control the time that the billboard will be displayed.
+ *
+ *
+ */
 
 
 public class Schedule implements Serializable {
+    // Setting the local variables that will be associated with the class
     private String          ScheduleName;
     private String          BillboardName;
     private LocalDate       date;
     private LocalTime       time;
     private Duration        duration;
 
-
+    //blank constructor
     public Schedule(){
         this.ScheduleName = null;
         this.BillboardName= null;
